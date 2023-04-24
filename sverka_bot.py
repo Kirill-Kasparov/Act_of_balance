11b4d6ae21aae110acf0f17eb7a4f1db922d77dc
kks23 <webphantom@yandex.ru>
2023-04-24 10:25:21 +0300

    Синхронизируем код

--- sverka_bot.py
+++ sverka_bot.py
@@ -3,6 +3,7 @@ import itertools
 import telebot
 import time
 import os
+import datetime
 
 with open('bot_token.TXT', 'r') as file:
     token = file.readline().strip()
@@ -17,19 +18,13 @@ def send_welcome(message):
     bot.send_message(message.from_user.id, 'Ссылка на отчет: http://kdw.komus.net/kdw/reps/paramreport.jsp?numrep=4177')
     bot.send_message(message.from_user.id, 'В оригинальном файле ничего не меняй!')
 
+
 @bot.message_handler(content_types=['document'])
 def handle_file(message):
-    # Скачиваем файл
-    file_info = bot.get_file(message.document.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    # Открываем файл как датафрейм pandas
-    df = pd.read_excel(downloaded_file)
-
     def search_for_orders(sverka_df):
         if len(sverka_df['№ заказа']) > 0:
             orders_list = sverka_df['№ заказа'].to_list()
             orders_list = list(set(orders_list))
-            sverka_df['Комментарий'] = '-'
             count_total = 0
             count_true = 0
             for order in orders_list:
@@ -39,8 +34,9 @@ def handle_file(message):
                         sverka_df.loc[sverka_df['№ заказа'] == order, 'Комментарий'] = 'Оплачено по заказу'
                         count_true += 1
                 count_total += 1
-            print('Опознаны оплаты по заказам:', count_true, '/ Всего заказов:', count_total)
+            # print('Опознаны оплаты по заказам:', count_true, '/ Всего заказов:', count_total)
         return sverka_df
+
     def search_by_amount(sverka_df):
         count_true = 0
         debet_db = sverka_df.loc[(sverka_df['Комментарий'] == '-') & (sverka_df['Дебет'] != 0)]
@@ -55,8 +51,9 @@ def handle_file(message):
                     sverka_df.at[result2.iloc[0].name, 'Комментарий'] = 'Найдено по сумме к платежу: ' + str(
                         sverka_df.at[result.iloc[0].name, '№ документа'])
                     count_true += 1
-            print('Опознаны оплаты по сумме:', count_true, '/ Всего заказов:', len(debet_list))
+            # print('Опознаны оплаты по сумме:', count_true, '/ Всего заказов:', len(debet_list))
         return sverka_df
+
     def search_by_combo(sverka_df, x=0):
         debet_db = sverka_df.loc[(sverka_df['Комментарий'] == '-') & (sverka_df['Дебет'] != 0.0)]
         credit_db = sverka_df.loc[(sverka_df['Комментарий'] == '-') & (sverka_df['Кредит'] != 0.0)]
@@ -113,6 +110,7 @@ def handle_file(message):
                 count_false += 1
             # print('Опознано комбинаций:', count_true, 'из', len(credit_list), 'платежей. Обработано:', count_false)
         return sverka_df
+
     def search_for_non_payments(sverka_df):
         debet_db = sverka_df.loc[(sverka_df['Комментарий'] == '-') & (sverka_df['Дебет'] != 0.0)]
         if len(debet_db['№ заказа']) > 0:
@@ -122,14 +120,41 @@ def handle_file(message):
             for order in orders_list:
                 if len(str(order)) > 8:
                     order_df = sverka_df.loc[(sverka_df['Комментарий'] == '-') & (sverka_df['№ заказа'] == order)]
-                    if order_df['Дебет'].sum() != order_df['Кредит'].sum() and order_df['Кредит'].sum() != 0:
+                    if order_df['Дебет'].sum() > order_df['Кредит'].sum() and order_df['Кредит'].sum() != 0.0:
                         sverka_df.loc[sverka_df['№ заказа'] == order, 'Комментарий'] = 'Недоплата: ' + str(
-                            order_df['Дебет'].sum() - order_df['Кредит'].sum()) + 'руб.'
+                            round(order_df['Дебет'].sum() - order_df['Кредит'].sum(), 2)) + ' руб.'
                         count_true += 1
-            print('Выявлено недоплат:', count_true)
+                    elif order_df['Дебет'].sum() < order_df['Кредит'].sum() and order_df['Кредит'].sum() != 0.0:
+                        sverka_df.loc[sverka_df['№ заказа'] == order, 'Комментарий'] = 'Переплата: ' + str(
+                            round(order_df['Кредит'].sum() - order_df['Дебет'].sum(), 2)) + ' руб.'
+                        count_true += 1
+            # print('Выявлено недоплат:', count_true)
         return sverka_df
 
+    def logs():
+        # логи
+        debet_db = sverka_df.loc[(sverka_df['Комментарий'] != '-') & (sverka_df['Дебет'] != 0)]
+        end_debet = len(debet_db['Дебет'])
+        user_logs = 'date: ' + str(datetime.datetime.today()) + ';' + 'user:' + str(message.from_user.id) + ';' + \
+                    'name:' + str(message.from_user.first_name) + ' ' + str(message.from_user.last_name) + ';' + \
+                    'username:' + str(message.from_user.username) + ';' + \
+                    'message:' + str(message.text) + ';' + 'file:' + str(
+            message.document.file_name) + ';' + 'order:' + str(start_debet) + ';' + 'order_detect:' + str(end_debet)
+        print(user_logs)
+
+        while True:  # проверка, если файл открыт
+            try:
+                save_user_logs = pd.Series(user_logs)
+                save_user_logs.to_csv('users_logs.csv', sep=';', encoding='windows-1251', index=False, mode='a',
+                                      header=False)
+                break
+            except UnicodeEncodeError:
+                break
+
     start = time.time()  # для таймера выполнения
+    # Скачиваем файл
+    file_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
 
     # Загрузка базы
     sverka_df = pd.read_excel(downloaded_file, sheet_name='сокращённый по ЮрЛицу', header=11, nrows=200000)
@@ -138,6 +163,9 @@ def handle_file(message):
     sverka_df = sverka_df.dropna(subset=['Дебет'])
     sverka_df = sverka_df.dropna(subset=['№ п/п'])
     sverka_df = sverka_df.iloc[:, 0: 8]
+    sverka_df['Комментарий'] = '-'
+    debet_db = sverka_df.loc[(sverka_df['Комментарий'] == '-') & (sverka_df['Дебет'] != 0)]
+    start_debet = len(debet_db['Дебет'])
 
     # запускаем проверку по заказам
     sverka_df = search_for_orders(sverka_df)
@@ -148,6 +176,9 @@ def handle_file(message):
     # запускаем проверку по комбинациям
     total_order = sverka_df.loc[(sverka_df['Комментарий'] == '-') & (sverka_df['Дебет'] != 0.0)]
     total_order = len(total_order['Дебет'])
+    if total_order > 500:
+        bot.send_message(message.from_user.id, "Выявлено " + str(
+            total_order) + " неопознанных платежей, обработка может занять несколько минут.")
     sverka_df = search_by_combo(sverka_df)
 
     # перезапускаем проверку на комбинации со смещением диапазона поиска
@@ -173,24 +204,27 @@ def handle_file(message):
     sverka_df = search_for_non_payments(sverka_df)
 
     # Завершаем обработку и сохраняем в файл
+    logs()
     end = time.time()
-    print("Время выполнения:", int(end - start), 'сек.')
+    print("Время выполнения: ", int(end - start), ' сек.')
     sverka_df.to_excel('Разбор_сальдо.xlsx', index=False)
 
-    bot.send_message(message.from_user.id, "Время выполнения:" + str(int(end - start)) + 'сек.')
+    bot.send_message(message.from_user.id, "Время выполнения: " + str(int(end - start)) + ' сек.')
     with open('Разбор_сальдо.xlsx', 'rb') as f:
         bot.send_document(message.chat.id, f)
 
-try:    # перезапуск при достижении лимита и дисконекте
+
+try:  # перезапуск при достижении лимита и дисконекте
     count_logs += 1
-    if count_logs == 200:    # перезапуск при достижении лимита
+    if count_logs == 200:  # перезапуск при достижении лимита
         bot.stop_polling()
-        crash_logs = pd.Series(str(pd.to_datetime(int(time.time() + 10800), unit='s')) + ';' + 'Достигнут лимит 200 запросов')
+        crash_logs = pd.Series(
+            str(pd.to_datetime(int(time.time() + 10800), unit='s')) + ';' + 'Достигнут лимит 200 запросов')
         crash_logs.to_csv('crash_logs.csv', sep=';', encoding='windows-1251', index=False, mode='a', header=False)
         os.startfile("Sverka (server for bot).exe")
 
     bot.polling(none_stop=True, interval=0)
-except:    # перезапуск при дисконекте
+except:  # перезапуск при дисконекте
     crash_logs = pd.Series(str(pd.to_datetime(int(time.time() + 10800), unit='s')) + ';' + 'Дисконнект')
     crash_logs.to_csv('crash_logs.csv', sep=';', encoding='windows-1251', index=False, mode='a', header=False)
     time.sleep(10)
